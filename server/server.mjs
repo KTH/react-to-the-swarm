6160a905a808cb059051671a9829e6a46ceb00db
tinglev <tinglev@kth.se>
2020-01-10 14:25:05 +0100

    Work work

--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import Docker from 'dockerode';
 import cors from 'cors';
+import stream from 'stream';
 
 const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 const app = express()
@@ -13,6 +14,32 @@ app.get('/services/:service_name/tasks', async (req, res, next) => {
             "filters": `{\"service\": [\"${req.params.service_name}\"]}`
         });
         res.json(tasks);
+    } catch (e) {
+        res.send('Error: ' + e);
+    }
+});
+
+app.get('/services/:service_name/logs', async (req, res, next) => {
+    try {
+        let services = await docker.listServices({
+            "filters": `{\"name\": [\"${req.params.service_name}\"]}`
+        });
+        if (services.length > 0) {
+            let service = await docker.getService(services[0]["ID"]);
+            let logStream = await service.logs({
+                "follow": true,
+                "stdout": true,
+                "stderr": true,
+            });
+            let trimStream = new stream.Writable({
+                write: function(chunk, _, next) {
+                    res.write(chunk.toString().substring(8));
+                    next();
+                }
+            });
+            res.setHeader('Content-Type', 'text/plain;charset=UTF-8');
+            logStream.pipe(trimStream);
+        }
     } catch (e) {
         res.send('Error: ' + e);
     }
--- src/Service.js
+++ src/Service.js
@@ -4,14 +4,27 @@ import { Card, Col } from 'react-bootstrap';
 function Service(props) {
 
     const [tasks, setTasks] = useState([]);
+    const [logData, setLogData] = useState('');
 
     useEffect(() => {
+        var subscription;
         const getTasks = async () => {
             var result = await fetch(`http://localhost:3001/services/${props.service.Spec.Name}/tasks`);
             var resultJson = await result.json();
             setTasks(resultJson);
         };
+        const getLogs = async() => {
+            var result = await fetch(`http://localhost:3001/services/${props.service.Spec.Name}/logs`);
+            var logReader = await result.body.getReader();
+            logReader.read().then(
+                function process({_, value}) {
+                    setLogData(d => d + new TextDecoder("utf-8").decode(value));
+                    return logReader.read().then(process); 
+                }
+            )
+        }
         getTasks();
+        getLogs();
     }, [props.service]);
 
     return (
@@ -23,6 +36,7 @@ function Service(props) {
                 <Card.Body>
                     Target replicas: {JSON.stringify(props.service.Spec.Mode.Replicated.Replicas)} <br />
                     Tasks: {tasks.length}
+                    Logs: {logData}
                 </Card.Body>
             </Card>
         </Col>
