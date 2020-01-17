1309d421fa0094328061afdd036b43a77bf006f6
tinglev <tinglev@kth.se>
2020-01-17 10:53:46 +0100

    Logging improvements

--- src/Service.js
+++ src/Service.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Button } from 'react-bootstrap';
 
 function Service(props) {
 
     const [tasks, setTasks] = useState([]);
     const [logData, setLogData] = useState('');
+    const [showLogs, setShowLogs] = useState(false); 
 
     useEffect(() => {
+        let logReader = null;
         const getTasks = async () => {
             var result = await fetch(`http://localhost:3001/services/${props.service.Spec.Name}/tasks`);
             var resultJson = await result.json();
@@ -14,17 +16,45 @@ function Service(props) {
         };
         const getLogs = async() => {
             var result = await fetch(`http://localhost:3001/services/${props.service.Spec.Name}/logs`);
-            var logReader = await result.body.getReader();
+            logReader = await result.body.getReader();
             logReader.read().then(
-                function process({_, value}) {
-                    setLogData(d => d + new TextDecoder("utf-8").decode(value));
+                function process({done, value}) {
+                    if (done) return;
+                    setLogData(d => new TextDecoder("utf-8").decode(value) + d);
                     return logReader.read().then(process); 
                 }
             )
         }
         getTasks();
-        getLogs();
-    }, [props.service]);
+        if (showLogs) {
+            getLogs();
+        }
+        else {
+            if (logReader !== null && logReader.closed) logReader.cancel();
+        }
+
+        // clean up
+        return (() => { 
+            if (logReader !== null && logReader.closed) logReader.cancel();
+        });
+    }, [props.service, showLogs]);
+
+    var logBlock = () => {
+        if (showLogs) {
+            return (
+                <code style={{fontSize: '10px', whiteSpace: 'pre'}}>
+                    {logData}
+                </code>
+            );
+        }
+        else {
+            return (<React.Fragment></React.Fragment>);
+        }
+    }
+
+    var handleLogClick = () => {
+        setShowLogs(!showLogs);
+    }
 
     return (
         <Col key={props.service.ID} xs={12 / props.columns}>
@@ -34,12 +64,12 @@ function Service(props) {
                 </Card.Header>
                 <Card.Body style={{maxHeight: '300px', overflowY: 'auto'}}>
                     <React.Fragment>
-                        REPLICAS: {tasks.length} / {JSON.stringify(props.service.Spec.Mode.Replicated.Replicas)} <br />
-                        LOGS: <br />
+                        REPLICAS: {tasks.length} / {JSON.stringify(props.service.Spec.Mode.Replicated.Replicas)}
+                        <br /><br />
+                        <Button onClick={handleLogClick}>LOGS</Button>
+                        <br />
                     </React.Fragment>
-                    <code style={{fontSize: '10px', whiteSpace: 'pre'}}>
-                        {logData}
-                    </code>
+                    {logBlock()}
                 </Card.Body>
             </Card>
         </Col>
